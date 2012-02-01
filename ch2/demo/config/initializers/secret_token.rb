ab208f9a094d83b8c8c612f56f7f138f6d4176fc
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-02-01 03:30:59 -0700

    Ch. 2 environment testing, start through Hello from Rails

---
+++ ch2/demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '4baaec830b09198d9d5ac365c425ffeaff54542810c8e74c264bce9557e3960b3b89471fb2926cf6dd77fc1ebe20dda0bc3d7f8bd3c5853d009bb6c5000ed31f'
