2ab008f13e2a914d84e4a62798b14b6fb8962af8
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-02-03 19:01:27 -0700

    project03 directory created

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project03::Application.config.secret_token = '8db1b2ae2b5c2313178e59b86521e4d2b5563832e8ba8055fd7214868cd30235d3118ce8c370f7aca30c709dbe380557502c53edd132084034ddc2ea25cf5fe0'
