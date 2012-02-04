06fa662dff9dc4e75ab72a5673aeeb39d9173ed7
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-02-03 19:45:25 -0700

    Iteration A1 complete

--- project03/config/initializers/secret_token.rb
+++ project03/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Project03::Application.config.secret_token = '8db1b2ae2b5c2313178e59b86521e4d2b5563832e8ba8055fd7214868cd30235d3118ce8c370f7aca30c709dbe380557502c53edd132084034ddc2ea25cf5fe0'
+Project03::Application.config.secret_token = 'b20a78171761095252d39d8459b84e1908d11727957b1a13042ed20e15166f08a2dae86e3d2601ea30a2615e67dca58cb2c7fe2d47e28feb3e62e5f97c2ac718'
