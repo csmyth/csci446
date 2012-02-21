50569f7b28f914a9a82234db7c6c1db8f06d068f
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-02-21 05:35:00 -0700

    New application and directories generated

---
+++ project04/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Articles::Application.config.secret_token = '7a9c21abf35010dd31fab87fb0f119af894bfa73dc8752ff569965045a67137f1ebf980176d34a65113f403574180a6be13b5e1deaa18522c635dbf88882cdc3'
