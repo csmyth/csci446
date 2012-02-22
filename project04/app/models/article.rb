3b4feed8d054e0c0e91a4c4b5981789cc393908f
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-02-21 18:33:26 -0700

    Added validation for article creation, including disallowing Pat

--- project04/app/models/article.rb
+++ project04/app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+	validates :title, :author, :body, presence: true
+	validates :title, uniqueness: true
+	validates :author, :exclusion => { 
+		:in => %w(Pat pat PAT PAt PaT pAT pAt paT), 
+		message: "Pat not allowed!" }
 end
