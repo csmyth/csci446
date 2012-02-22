7e79be28f9a7e1a804fd3f71d33e5a7199316c11
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-02-22 04:24:43 -0700

    Updated routes, listing as index, articl.es redirect

--- project04/config/routes.rb
+++ project04/config/routes.rb
@@ -13,6 +13,7 @@ Articles::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
+  match 'articl.es' => 'listing#index', as: 'listing'
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
@@ -53,6 +54,7 @@ Articles::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'listing#index', as: 'listing'
 
   # See how all your routes lay out with "rake routes"
 
