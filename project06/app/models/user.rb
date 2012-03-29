d61fb97ca56adc9a8ef2db540e2106e028e6eb10
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-03-29 10:11:25 -0600

    Update admin model to user model, add role validation

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,5 +1,8 @@
-class Admin < ActiveRecord::Base
+class User < ActiveRecord::Base
+	ROLES = {"admin", "member", "public"}
+	
 	validates :username, presence: true, uniqueness: true
 	validates :email, :email => true
+	validates :role, inclusion: ROLES
 	has_secure_password
 end
