121a09dc391f76f8ac1037b9e9355913a323e188
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-03-28 23:21:29 -0600

    gemfile updated to utilize bcrypt-ruby

--- project06/db/seeds.rb
+++ project06/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Admin.delete_all
+
+Admin.create(name: "administrator", password: "password")
