77339395b825fbe1a2b5db4f2e02b43792579c13
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-04-09 00:35:16 -0600

    Tried to implement recaptcha, problems with verification, line in users_controller under create action that has verify_recaptcha causing problems, note users still created despite error

---
+++ project06/config/initializers/recaptcha.rb
@@ -0,0 +1,5 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6Ldz_M8SAAAAAEUMQ2rUE808ZHHEmIftJgzQc6AQ'
+  config.private_key = '6Ldz_M8SAAAAAGrhXv2aFfcZUWujsDOrxQnYuNXs'
+  config.proxy = 'http://myproxy.com.au:8080'
+end
