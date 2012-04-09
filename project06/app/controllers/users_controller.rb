0c3cebd395b3c86146c6470d025ca9029a9f14a8
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-04-08 23:21:48 -0600

    Updated user partial form with password confirmation and corrected notice displayed after registering

--- project06/app/controllers/users_controller.rb
+++ project06/app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_url, notice: 'Welcome, #{@user.first_name} #{@user.last_name}' }
+        format.html { redirect_to users_url, notice: "Welcome, #{@user.first_name} #{@user.last_name}" }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
