108235986b164ccc7ae800d9f5b88b405577f92b
Cecilia Smyth <csmyth@mymail.mines.edu>
2012-04-08 21:32:41 -0600

    Added validations for user data

--- project06/app/models/user.rb
+++ project06/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+	ROLES = ["member", "admin"]
+	
+	validates :username, presence: true, uniqueness: true, length: {minimum: 6}
+	validates :password, length: {minimum: 6}
+	validates :first_name, :last_name, :email, presence: true
+	validates :email, :email => true
+	validates :role, inclusion: ROLES
+	has_secure_password
 end
